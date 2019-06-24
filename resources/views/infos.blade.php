fe0302377ebe5c460fe4cffaa10375ffff0dbf71
Moise Destin <moisedestin@gmail.com>
2019-06-24 17:06:32 -0400

    add infos page arranjman

--- resources/views/infos.blade.php
+++ resources/views/infos.blade.php
@@ -22,22 +22,34 @@
             color:black;
             background-color: white;
         }
-        @media (min-width: 1200px)
+
+        @media (min-width: 576px){
             .container {
-                max-width: 1140px;
+                max-width: 540px;
             }
-            @media (min-width: 992px)
+        }
+
+        @media (min-width: 768px){
+            .container {
+                max-width: 720px;
+            }
+        }
+            @media (min-width: 992px){
                 .container {
                     max-width: 960px;
                 }
-                @media (min-width: 768px)
-                    .container {
-                        max-width: 720px;
-                    }
-                    @media (min-width: 576px)
-                        .container {
-                            max-width: 540px;
-                        }
+            }
+
+
+
+
+
+        @media (min-width: 1200px){
+            .container {
+                max-width: 1140px;
+            }
+        }
+
                         .container {
                             width: 100%;
                             padding-right: 15px;
